7272518f0f3648f12d712045e39c5f9cc643604b
JhonatanZ13 <zambranojhonatan20@gmail.com>
2021-09-02 17:56:20 -0500

    Plantilla MVC

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    include_once '../lib/helpers.php';
+    include_once '../lib/helpersLogin.php';
+
+    	if (isset($_GET['modulo'])) {
+       	    resolve();
+    	}
+
+    include_once '../view/partials/scripts.php';
+?>
+    
