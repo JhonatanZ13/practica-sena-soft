74d23e402013f2ce5e000b36c429f70b6b9caf73
JhonatanZ13 <zambranojhonatan20@gmail.com>
2021-09-05 12:08:21 -0500

    Prueba borrador de factura

---
+++ view/partials/scripts.php
@@ -0,0 +1,6 @@
+<script src="./js/jquery/jquery-3.6.0.js"></script>
+<script src="./js/bootstrap/bootstrap.js"></script>
+<script src="./js/bootstrap/bootstrap.bundle.js"></script>
+<script src="./js/vue/vue.js"></script>
+<script src="./js/main.js"></script>
+</html>
--- web/index.php
+++ web/index.php
@@ -1,12 +1,14 @@
 <?php
 
     include_once '../lib/helpers.php';
-    include_once '../lib/helpersLogin.php';
-
-    	if (isset($_GET['modulo'])) {
-       	    resolve();
-    	}
+    include_once '../view/partials/header.php';
+    
+    echo "<body>";
+        include_once '../view/facturas/prueba_factura.php';
+        if (isset($_GET['modulo'])) {
+            resolve();
+        }
+    echo "</body>";
 
     include_once '../view/partials/scripts.php';
-?>
-    
+    include_once '../view/partials/scripts.php';
