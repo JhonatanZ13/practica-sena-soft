8566ae81dfc06cfc674c54d84eff53159300b340
JhonatanZ13 <zambranojhonatan20@gmail.com>
2021-09-11 19:09:38 -0500

    Pequeño cambio añadido

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -3,9 +3,16 @@ window.addEventListener('DOMContentLoaded', event => {
     if (sidebarToggle) {
         sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
+            
+            if($("#toogleButton").hasClass("fa-chevron-left")){
+                $("#toogleButton").removeClass("fa-chevron-left");
+                $("#toogleButton").addClass("fa-chevron-right");
+            }else{
+                $("#toogleButton").removeClass("fa-chevron-right");
+                $("#toogleButton").addClass("fa-chevron-left");
+            }
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
-
 });
