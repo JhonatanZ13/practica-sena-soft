94bd9d914294ef47b99eaab1890fe1d15ef28612
JhonatanZ13 <zambranojhonatan20@gmail.com>
2021-09-11 19:02:45 -0500

    Agregue plantilla simple con sidebar

--- web/index.php
+++ web/index.php
@@ -3,12 +3,10 @@
     include_once '../lib/helpers.php';
     include_once '../view/partials/header.php';
     
-    echo "<body>";
-        include_once '../view/facturas/prueba_factura.php';
+        //include_once '../view/facturas/prueba_factura.php';
         if (isset($_GET['modulo'])) {
             resolve();
         }
-    echo "</body>";
 
-    include_once '../view/partials/scripts.php';
+    include_once '../view/partials/footer.php';
     include_once '../view/partials/scripts.php';
